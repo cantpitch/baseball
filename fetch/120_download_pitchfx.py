013ad08b114ca732d3219931d741a2c9a4e4b93e
Hans Van Slooten <vansloot@gmail.com>
2017-07-16 11:16:55 -0500

    readme; organization

---
+++ fetch/120_download_pitchfx.py
@@ -0,0 +1,43 @@
+import argparse
+import urllib.request
+from os import makedirs
+
+parser = argparse.ArgumentParser(description='Download PitchF/X files')
+parser.add_argument('-y', '--year', type=int, help='the year to fetch')
+args = parser.parse_args()
+
+
+
+event_files = ['1920seve.zip', '1930seve.zip', '1940seve.zip', '1950seve.zip', 
+               '1960seve.zip', '1970seve.zip', '1980seve.zip', '1990seve.zip',
+               '2000seve.zip', '2010seve.zip', '1910sbox.zip', '1920sbox.zip', 
+               '1930sbox.zip', '1940sbox.zip', '1950sbox.zip', 'allas.zip',
+               'allpost.zip']
+
+gamelogs_start = 1871
+gamelogs_end   = 2016
+
+post_gamelogs = ['glws.zip', 'glas.zip', 'glwc.zip', 'gldv.zip', 'gllc.zip']
+
+makedirs('../data/pitchfx', exist_ok=True)
+
+url = 'http://www.retrosheet.org'
+
+for file in event_files:
+    src = url + '/events/' + file
+    dst = '../zips/' + file
+    print("Downloading {0} to {1}".format(src, dst))
+    urllib.request.urlretrieve(src, dst)
+
+for year in range(gamelogs_start, gamelogs_end + 1):
+    file = 'gl' + str(year) + '.zip'
+    src = url + '/gamelogs/' + file
+    dst = '../zips/' + file
+    print("Downloading {0} to {1}".format(src, dst))
+    urllib.request.urlretrieve(src, dst)
+
+for file in post_gamelogs:
+    src = url + '/gamelogs/' + file
+    dst = '../zips/' + file
+    print("Downloading {0} to {1}".format(src, dst))
+    urllib.request.urlretrieve(src, dst)
--- readme.md
+++ readme.md
@@ -8,3 +8,29 @@ A collection of Bash, Python, and SQL scripts for building and maintaining a Pos
 * __fetch__ - scripts for downloading data from various internet sources.
 * __lib__ - Python libraries
 * __misc__ - various scripts for research
+
+## Installation and Setup
+
+This project requires a few different things to be setup before it's useful.
+
+* Python 3 installed (development is done with Python 3.6+). This
+can be installed in many different ways and is outside of the scope of this document 
+to explain.
+* A PostgreSQL database (currently called _baseball_) created. This can be
+a non-trivial undertaking, but on MacOS it is recommended that you use __Postgres.app__
+available at http://postgresapp.com/ to get up and running quickly.
+* Chadwick tools for processing the Retrosheet files http://chadwick.sourceforge.net/doc/index.html
+
+Once that is all setup, it's recommended that you create a virtualenv environment for
+development:
+
+    mkdir ~/Projects/venvs
+    cd ~/Projects/venvs
+    virtualenv toolsofignorance
+    source toolsofignorance/bin/activate
+
+In order to use the local libraries add a `.pth` file in 
+`venvs/toolsofignorance/lib/python3.6/site-packages` called `baseball.pth` with the path
+to the `toolsofignorance/lib` directory:
+
+    /Users/hvs/Projects/toolsofignorance/lib
